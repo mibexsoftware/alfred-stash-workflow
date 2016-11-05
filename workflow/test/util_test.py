3d499f93f9b0502547d01db1901f95afbc59f1d9
Michael Rüegg <rueegg.michael@gmail.com>
2016-11-05 14:36:13 +0100

    Using utcfromtimestamp to fix date based tests on travis

--- workflow/test/util_test.py
+++ workflow/test/util_test.py
@@ -10,10 +10,10 @@ from src.util import pretty_date
 class UtilTest(TestCase):
 
     def test_pretty_timestamp(self):
-        mytime = pytz.utc.localize(datetime.fromtimestamp(1478187004))  # Thu, 03 Nov 2016 15:30:04 GMT
-        self.assertEquals('19 hours ago', pretty_date(mytime, now=datetime(2016, 11, 04, 12, 0, 0, 0, pytz.UTC)))
-        self.assertEquals('Yesterday', pretty_date(mytime, now=datetime(2016, 11, 05, 12, 0, 0, 0, pytz.UTC)))
-        self.assertEquals('2 days ago', pretty_date(mytime, now=datetime(2016, 11, 06, 12, 0, 0, 0, pytz.UTC)))
-        self.assertEquals('1 week(s) ago', pretty_date(mytime, now=datetime(2016, 11, 11, 12, 0, 0, 0, pytz.UTC)))
-        self.assertEquals('1 month(s) ago', pretty_date(mytime, now=datetime(2016, 12, 11, 12, 0, 0, 0, pytz.UTC)))
-        self.assertEquals('1 year(s) ago', pretty_date(mytime, now=datetime(2017, 11, 11, 12, 0, 0, 0, pytz.UTC)))
+        my_time = pytz.utc.localize(datetime.utcfromtimestamp(1478187004))  # Thu, 03 Nov 2016 15:30:04 GMT
+        self.assertEquals('20 hours ago', pretty_date(my_time, now=datetime(2016, 11, 04, 12, 0, 0, 0, pytz.UTC)))
+        self.assertEquals('Yesterday', pretty_date(my_time, now=datetime(2016, 11, 05, 12, 0, 0, 0, pytz.UTC)))
+        self.assertEquals('2 days ago', pretty_date(my_time, now=datetime(2016, 11, 06, 12, 0, 0, 0, pytz.UTC)))
+        self.assertEquals('1 week(s) ago', pretty_date(my_time, now=datetime(2016, 11, 11, 12, 0, 0, 0, pytz.UTC)))
+        self.assertEquals('1 month(s) ago', pretty_date(my_time, now=datetime(2016, 12, 11, 12, 0, 0, 0, pytz.UTC)))
+        self.assertEquals('1 year(s) ago', pretty_date(my_time, now=datetime(2017, 11, 11, 12, 0, 0, 0, pytz.UTC)))
